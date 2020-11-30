11689c024719a4a5720de1c6eaec0b1a2f302d5b
wormhole <363408268@qq.com>
2020-11-30 09:46:15 +0800

    FastcallReference注解

---
+++ fastcall-core/src/main/java/net/stackoverflow/fastcall/annotation/FastcallReference.java
@@ -0,0 +1,16 @@
+package net.stackoverflow.fastcall.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 服务引用注解
+ *
+ * @author wormhole
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FastcallReference {
+
+    String group() default "default";
+}
