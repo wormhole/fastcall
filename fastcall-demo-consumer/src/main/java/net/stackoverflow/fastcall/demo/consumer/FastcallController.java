fbe60f9f23d92ea4602356508f44c330469f1c8b
wormhole <363408268@qq.com>
2020-11-30 18:33:58 +0800

    consumer demo

---
+++ fastcall-demo-consumer/src/main/java/net/stackoverflow/fastcall/demo/consumer/FastcallController.java
@@ -0,0 +1,19 @@
+package net.stackoverflow.fastcall.demo.consumer;
+
+import net.stackoverflow.fastcall.annotation.FastcallReference;
+import net.stackoverflow.fastcall.demo.api.SayService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FastcallController {
+
+    @FastcallReference
+    private SayService sayService;
+
+    @GetMapping("/say")
+    public String say(@RequestParam("content") String content) {
+        return sayService.say(content);
+    }
+}
---
+++ fastcall-demo-consumer/src/main/java/net/stackoverflow/fastcall/demo/consumer/FastcallDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package net.stackoverflow.fastcall.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcallDemoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcallDemoConsumerApplication.class, args);
+	}
+
+}
