d506c2071bf6859d2f87316d5238ab02cab8c4c3
wormhole <363408268@qq.com>
2020-12-02 00:27:34 +0800

    添加marshalling依赖

---
+++ fastcall-core/src/main/java/net/stackoverflow/fastcall/io/serial/MarshallingCodecFactory.java
@@ -0,0 +1,28 @@
+package net.stackoverflow.fastcall.io.serial;
+
+import io.netty.handler.codec.marshalling.*;
+import org.jboss.marshalling.MarshallerFactory;
+import org.jboss.marshalling.Marshalling;
+import org.jboss.marshalling.MarshallingConfiguration;
+
+public class MarshallingCodecFactory {
+
+    public static MarshallingDecoder buildMarshallingDecoder() {
+        MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        UnmarshallerProvider provider = new DefaultUnmarshallerProvider(marshallerFactory, configuration);
+        MarshallingDecoder decoder = new MarshallingDecoder(provider, 1024 * 1024 * 1);
+        return decoder;
+    }
+
+    public static MarshallingEncoder buildMarshallingEncoder() {
+        MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        MarshallerProvider provider = new DefaultMarshallerProvider(marshallerFactory, configuration);
+        MarshallingEncoder encoder = new MarshallingEncoder(provider);
+        return encoder;
+    }
+
+}
