cdb4b61f0e002125856ce6ef9c82139e75e750a3
wormhole <363408268@qq.com>
2020-12-02 19:15:57 +0800

    @EnableFastcall

--- fastcall-demo-consumer/src/main/java/net/stackoverflow/fastcall/demo/consumer/FastcallDemoConsumerApplication.java
+++ fastcall-demo-consumer/src/main/java/net/stackoverflow/fastcall/demo/consumer/FastcallDemoConsumerApplication.java
@@ -1,11 +1,9 @@
 package net.stackoverflow.fastcall.demo.consumer;
 
-import net.stackoverflow.fastcall.annotation.EnableFastcall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableFastcall
 public class FastcallDemoConsumerApplication {
 
 	public static void main(String[] args) {
--- fastcall-demo-provider/src/main/java/net/stackoverflow/fastcall/demo/provider/FastcallDemoProviderApplication.java
+++ fastcall-demo-provider/src/main/java/net/stackoverflow/fastcall/demo/provider/FastcallDemoProviderApplication.java
@@ -1,9 +1,11 @@
 package net.stackoverflow.fastcall.demo.provider;
 
+import net.stackoverflow.fastcall.annotation.EnableFastcall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFastcall
 public class FastcallDemoProviderApplication {
 
 	public static void main(String[] args) {
