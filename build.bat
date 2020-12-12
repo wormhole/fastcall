5c021f250dd3e562f94da6fb753d9882d233518d
wormhole <363408268@qq.com>
2020-12-12 09:56:23 +0800

    build.bat

--- build.bat
+++ build.bat
@@ -1,22 +1,15 @@
 cd fastcall-parent
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-core
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-spring-boot-autoconfigure
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-spring-boot-starter
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-demo-api
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-demo-provider
-call mvn clean
-call mvn install
+call mvn clean install
 cd ../fastcall-demo-consumer
-call mvn clean
-call mvn install
+call mvn clean install
 cd ..
