c96ad12961a45a04de38879b555e8fab0f5cbd4d
wormhole <363408268@qq.com>
2020-12-22 18:43:47 +0800

    jedis.close()

--- fastcall-core/src/main/java/net/stackoverflow/fastcall/registry/redis/RedisRegistryManager.java
+++ fastcall-core/src/main/java/net/stackoverflow/fastcall/registry/redis/RedisRegistryManager.java
@@ -79,6 +79,7 @@ public class RedisRegistryManager extends AbstractRegistryManager {
                 metaDataList.add(metaData);
             }
         }
+        jedis.close();
         cache.setCache(latestCache);
     }
 
