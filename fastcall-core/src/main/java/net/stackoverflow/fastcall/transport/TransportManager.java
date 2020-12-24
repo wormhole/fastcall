20f2cfa312e93c8a24f209549d7f61e03a117b16
wormhole <363408268@qq.com>
2020-12-24 22:13:26 +0800

    TransportManager

--- fastcall-core/src/main/java/net/stackoverflow/fastcall/transport/TransportManager.java
+++ fastcall-core/src/main/java/net/stackoverflow/fastcall/transport/TransportManager.java
@@ -10,7 +10,24 @@ import java.net.SocketAddress;
  */
 public interface TransportManager {
 
+    /**
+     * 绑定本地端口
+     *
+     * @param localSocketAddress
+     */
     void bind(InetSocketAddress localSocketAddress);
 
+    /**
+     * 发送报文给远程地址
+     *
+     * @param remoteSocketAddress
+     * @param message
+     * @param <T>
+     */
     <T> void sendTo(InetSocketAddress remoteSocketAddress, T message);
+
+    /**
+     * 关闭所有连接与线程池
+     */
+    void close();
 }
--- fastcall-core/src/main/java/net/stackoverflow/fastcall/transport/fastcall/FastcallTransportManager.java
+++ fastcall-core/src/main/java/net/stackoverflow/fastcall/transport/fastcall/FastcallTransportManager.java
@@ -1,10 +1,12 @@
 package net.stackoverflow.fastcall.transport.fastcall;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import net.stackoverflow.fastcall.context.BeanContext;
 import net.stackoverflow.fastcall.context.ResponseFutureContext;
@@ -13,6 +15,9 @@ import net.stackoverflow.fastcall.serialize.SerializeManager;
 import net.stackoverflow.fastcall.transport.TransportManager;
 import net.stackoverflow.fastcall.transport.fastcall.codec.MessageDecoder;
 import net.stackoverflow.fastcall.transport.fastcall.codec.MessageEncoder;
+import net.stackoverflow.fastcall.transport.fastcall.handler.client.ClientAuthHandler;
+import net.stackoverflow.fastcall.transport.fastcall.handler.client.ClientHeatBeatHandler;
+import net.stackoverflow.fastcall.transport.fastcall.handler.client.ClientRpcHandler;
 import net.stackoverflow.fastcall.transport.fastcall.handler.server.ServerAuthHandler;
 import net.stackoverflow.fastcall.transport.fastcall.handler.server.ServerHeatBeatHandler;
 import net.stackoverflow.fastcall.transport.fastcall.handler.server.ServerRpcHandler;
@@ -20,7 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.util.Map;
 import java.util.concurrent.*;
 
 /**
@@ -46,11 +51,16 @@ public class FastcallTransportManager implements TransportManager {
 
     private final ExecutorService connectionExecutorService;
 
+    private final Map<String, Channel> channelPool;
+
+    private Channel sChannel;
+
     public FastcallTransportManager(SerializeManager serializeManager, BeanContext beanContext, ResponseFutureContext responseFutureContext, int nThreads, int maxConnection) {
         this.serializeManager = serializeManager;
         this.beanContext = beanContext;
         this.responseFutureContext = responseFutureContext;
 
+        this.channelPool = new ConcurrentHashMap<>();
         this.serverExecutorService = Executors.newSingleThreadExecutor(new NameThreadFactory("Server"));
         this.rpcExecutorService = new ThreadPoolExecutor(0, nThreads, 60, TimeUnit.SECONDS, new SynchronousQueue<>(), new NameThreadFactory("Rpc"));
         this.connectionExecutorService = new ThreadPoolExecutor(0, maxConnection, 60, TimeUnit.SECONDS, new SynchronousQueue<>(), new NameThreadFactory("Connection"));
@@ -79,8 +89,8 @@ public class FastcallTransportManager implements TransportManager {
                         });
                 ChannelFuture channelFuture = bootstrap.bind(localSocketAddress).sync();
                 log.info("[L:{}] TransportManager bind success", localSocketAddress.getAddress().getHostAddress() + ":" + localSocketAddress.getPort());
-                Channel channel = channelFuture.channel();
-                channel.closeFuture().sync();
+                sChannel = channelFuture.channel();
+                sChannel.closeFuture().sync();
             } catch (InterruptedException e) {
                 log.error("[L:{}] TransportManager fail to bind", localSocketAddress.getAddress().getHostAddress() + ":" + localSocketAddress.getPort(), e);
             } finally {
@@ -93,7 +103,83 @@ public class FastcallTransportManager implements TransportManager {
 
     @Override
     public <T> void sendTo(InetSocketAddress remoteSocketAddress, T message) {
+        String host = remoteSocketAddress.getAddress().getHostAddress();
+        Integer port = remoteSocketAddress.getPort();
+        String key = host + ":" + port;
+        Channel channel = channelPool.get(key);
+        if (channel == null || !channel.isActive()) {
+            channel = initChannel(remoteSocketAddress);
+        }
+        channel.writeAndFlush(message);
+    }
 
+    @Override
+    public synchronized void close() {
+        for (Channel channel : channelPool.values()) {
+            channel.close();
+        }
+        connectionExecutorService.shutdown();
+
+        rpcExecutorService.shutdown();
+        if (sChannel != null) {
+            sChannel.close();
+        }
+        rpcExecutorService.shutdown();
+    }
+
+    /**
+     * 初始化Channel
+     *
+     * @param remoteSocketAddress
+     * @return
+     */
+    private synchronized Channel initChannel(InetSocketAddress remoteSocketAddress) {
+        String host = remoteSocketAddress.getAddress().getHostAddress();
+        Integer port = remoteSocketAddress.getPort();
+        String key = host + ":" + port;
+
+        Channel channel = channelPool.get(key);
+        if (channel == null || !channel.isActive()) {
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+            connectionExecutorService.execute(() -> {
+                Bootstrap bootstrap = new Bootstrap();
+                EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+                try {
+                    bootstrap.group(eventLoopGroup)
+                            .channel(NioSocketChannel.class)
+                            .option(ChannelOption.TCP_NODELAY, true)
+                            .handler(new ChannelInitializer<SocketChannel>() {
+                                @Override
+                                protected void initChannel(SocketChannel socketChannel) {
+                                    ChannelPipeline pipeline = socketChannel.pipeline();
+                                    pipeline.addLast(new MessageDecoder(serializeManager));
+                                    pipeline.addLast(new MessageEncoder(serializeManager));
+                                    pipeline.addLast(new ReadTimeoutHandler(TIMEOUT));
+                                    pipeline.addLast(new ClientAuthHandler());
+                                    pipeline.addLast(new ClientHeatBeatHandler());
+                                    pipeline.addLast(new ClientRpcHandler(responseFutureContext));
+                                }
+                            });
+                    ChannelFuture channelFuture = bootstrap.connect(remoteSocketAddress).sync();
+                    log.info("[L:{} R:{}] TransportManager connect success", host, port);
+                    Channel chl = channelFuture.channel();
+                    channelPool.put(key, chl);
+                    countDownLatch.countDown();
+                    chl.closeFuture().sync();
+                } catch (InterruptedException e) {
+                    log.error("[R:{}] TransportManager fail to connect", host + ":" + port, e);
+                } finally {
+                    log.info("[R:{}] TransportManager closed", host + ":" + port);
+                    eventLoopGroup.shutdownGracefully();
+                }
+            });
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                log.error("[R:{}] TransportManager fail to init channel", host + ":" + port, e);
+            }
+        }
+        return channelPool.get(key);
     }
 
 }
